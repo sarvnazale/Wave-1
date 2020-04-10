5bdbf9a303ae952888b34405d46368f2dd9e74b1
sarvnazale <sarvnazale@gmail.com>
2020-04-10 16:30:44 -0400

    Question 3

--- Compound Interest.py
+++ Compound Interest.py
@@ -1,7 +1,8 @@
 deposit = float(input("Your initial amount of deposit is: "))
-year_one = deposit * 1.04
-year_two = year_one * 1.04
-year_three = year_two * 1.04
-print("End of year one: " + str(round(year_one, 2)))
-print("End of year two: " + str(round(year_two, 2)))
-print("End of year three: " + str(round(year_three, 2)))
+interest = 1.04
+year_one = deposit * interest
+year_two = year_one * interest
+year_three = year_two * interest
+print("End of year one: " + "$" + str(round(year_one, 2)))
+print("End of year two: " + "$" + str(round(year_two, 2)))
+print("End of year three: " + "$" + str(round(year_three, 2)))
